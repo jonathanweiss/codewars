4249ac7c50d0340fd896296ced79840eabe13135
Jonathan Weiß <jonnytrap@mysterycity.de>
2017-04-25 09:23:58 +0000

    Improve naming

--- good-vs-evil.js
+++ good-vs-evil.js
@@ -3,7 +3,7 @@
 function goodVsEvil(good, evil){
   const calc = (lookup, result, n, i) => (+result) + (n * lookup[i]);
   
-  const results = [
+  const messages = [
     'Battle Result: No victor on this battle field',
     'Battle Result: Good triumphs over Evil',
     'Battle Result: Evil eradicates all trace of Good',
@@ -12,5 +12,5 @@ function goodVsEvil(good, evil){
   const result = good.split(' ').reduce(calc.bind(null, [1, 2, 3, 3, 4, 10])) -
                  evil.split(' ').reduce(calc.bind(null, [1, 2, 2, 2, 3, 5, 10]))
 
-  return results[result === 0 ? 0 : result > 0 ? 1 : 2];
+  return messages[result === 0 ? 0 : result > 0 ? 1 : 2];
 }
