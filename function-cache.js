528d475665341362d55c37669371b4a8e0dba55a
Jonathan Weiß <jonnytrap@mysterycity.de>
2017-05-08 08:24:28 +0000

    Add solution for function cache

---
+++ function-cache.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/function-cache
+
+function cache(func) {
+  const cachedResults = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+
+    if (!(key in cachedResults)) {
+      const result = func.apply(null, args);
+      cachedResults[key] = result;
+    }
+
+    return cachedResults[key];
+  };
+}
